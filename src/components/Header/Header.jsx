9f769a7dc60759ce83a014ee2c7fc41400f25b07
RedQueen <83107940+aprilrushh@users.noreply.github.com>
2025-05-21 00:27:44 +0900

    Refactor header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useTheme } from '../../contexts/ThemeContext';
+import Navigation from '../Navigation/Navigation';
+import './Header.css';
+
+const Header = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <header className={`header ${theme}`}>
+      <div className="header-left">
+        <Link to="/" className="logo">
+          <img src="/images/ubex-logo.svg" alt="UBEX" className="logo-image" />
+        </Link>
+        <Navigation />
+      </div>
+      <div className="header-right">
+        <button onClick={toggleTheme} className="theme-toggle">
+          {theme === 'light' ? '🌙' : '☀️'}
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
