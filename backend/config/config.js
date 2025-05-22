52d377a9729a1f3908af8680b0a36b287b1c90c1
RedQueen <83107940+aprilrushh@users.noreply.github.com>
2025-05-22 16:24:15 +0900

    Update DB credentials for Docker MySQL

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,27 +2,27 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'ubex_exchange_dev',
+    username: process.env.DB_USER || 'mysql',
+    password: process.env.DB_PASS || 'mysql',
+    database: process.env.DB_NAME || 'ubex',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     logging: false
   },
   test: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'ubex_exchange_test',
+    username: process.env.DB_USER || 'mysql',
+    password: process.env.DB_PASS || 'mysql',
+    database: process.env.DB_NAME || 'ubex',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     logging: false
   },
   production: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || 'ubex_exchange_prod',
+    username: process.env.DB_USER || 'mysql',
+    password: process.env.DB_PASS || 'mysql',
+    database: process.env.DB_NAME || 'ubex',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     logging: false
   }
-}; 
+};
