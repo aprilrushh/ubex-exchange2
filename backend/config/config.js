f551d899006a7bfef91963ec520e59d8b2d16b77
aprilrushh <aprilrushh@gmail.com>
2025-05-24 16:37:17 +0900

    Auto sync: Sat May 24 16:37:17 JST 2025

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'mysql',
-    password: process.env.DB_PASS || 'mysql',
+    username: process.env.DB_USER || 'ubex_user',
+    password: process.env.DB_PASS || 'ubex1234',
     database: process.env.DB_NAME || 'ubex',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     logging: false
   },
   test: {
-    username: process.env.DB_USER || 'mysql',
-    password: process.env.DB_PASS || 'mysql',
+    username: process.env.DB_USER || 'ubex_user',
+    password: process.env.DB_PASS || 'ubex1234',
     database: process.env.DB_NAME || 'ubex',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     logging: false
   },
   production: {
-    username: process.env.DB_USER || 'mysql',
-    password: process.env.DB_PASS || 'mysql',
+    username: process.env.DB_USER || 'ubex_user',
+    password: process.env.DB_PASS || 'ubex1234',
     database: process.env.DB_NAME || 'ubex',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
